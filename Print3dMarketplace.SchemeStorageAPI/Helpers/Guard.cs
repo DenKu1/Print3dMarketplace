8da6e1c6e5ddcc770f218d4e9c0855b46c7ff0bf
dkulyk <LeDar54@gmail.com>
2023-12-17 18:32:15 +0200

    Cleaning up the project SchemeStorageAPI.

--- Print3dMarketplace.SchemeStorageAPI/Helpers/Guard.cs
+++ Print3dMarketplace.SchemeStorageAPI/Helpers/Guard.cs
@@ -2,11 +2,21 @@
 using Print3dMarketplace.SchemeStorageAPI.Contracts.DTOs;
 using Print3dMarketplace.SchemeStorageAPI.Helpers.Interfaces;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Print3dMarketplace.SchemeStorageAPI.Helpers;
 
 public class Guard : IGuard
 {
+	public void ValidateFileName(string fileName)
+	{
+		Regex regex = new Regex(@"[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/[^/]+\.stl
+									_[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}");
+		
+		if (fileName.IsNullOrEmpty() || !regex.Match(fileName).Success)
+			throw new ValidationException("fileName have another Pattern");
+	}
+
 	public void ValidateStlModel(StlSchemeRequestDTO stlScheme)
 	{
 		if (stlScheme is null
--- Print3dMarketplace.SchemeStorageAPI/Services/Interfaces/ISchemeStorageService.cs
+++ Print3dMarketplace.SchemeStorageAPI/Services/Interfaces/ISchemeStorageService.cs
@@ -3,7 +3,7 @@
 namespace Print3dMarketplace.SchemeStorageAPI.Services.Interfaces;
 
 /// <summary>
-///
+/// ISchemeStorageService that contains method for working with schemes 
 /// </summary>
 public interface ISchemeStorageService
 {
@@ -13,4 +13,11 @@ public interface ISchemeStorageService
 	/// <param name="stlScheme"></param>
 	/// <returns></returns>
 	public Task<string> UploadScheme(StlSchemeRequestDTO stlScheme);
+
+	/// <summary>
+	/// Donwload file by fileName
+	/// </summary>
+	/// <param name="fileName">fileName that correspond pattern</param>
+	/// <returns>Byte array that can be convert into file</returns>
+	public Task<byte[]> DownloadScheme(string fileName);
 }
